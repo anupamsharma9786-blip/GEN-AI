4218750b742d57e84fb883b0c48b4cc9d82127be
Anupam <anupamsharma9786@gmail.com>
2026-06-22 01:31:59 +0530

    login and register

--- Insta-Frontend/src/features/auth/pages/Login.jsx
+++ Insta-Frontend/src/features/auth/pages/Login.jsx
@@ -1,17 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/form.scss"
+import { Link } from 'react-router'
+import axios from 'axios'
+
 
 const Login = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        const response = await axios.post("http://localhost:3000/api/auth/login", {
+            username,
+            email,
+            password
+        },
+        {withCredentials: true}
+        ) 
+
+        console.log(response.data)
+    }
+
   return (
     <main>
         <div className="form-container">
             <h1>INSTY</h1>
             <h2>Login</h2>
-            <form>
-                <input type='text' name='username' placeholder='Enter Username' required />
-                <input type='password' name='password' placeholder='Enter Password' required />
+            <form onSubmit={handleSubmit}>
+                <input 
+                onInput={(e)=>{ setUsername(e.target.value)
+                    setEmail(e.target.value)
+                }}
+                type='text' 
+                name='username' 
+                placeholder='Enter Username or Email' 
+                required />
+                <input 
+                onInput={(e)=>{ setPassword(e.target.value)}}
+                type='password' 
+                name='password' 
+                placeholder='Enter Password' 
+                required />
                 <button type='submit'>Login</button>
             </form>
+            <p className='toggleForm'>Don't have an account? <Link className="link" to="/register">Register</Link></p>
         </div>
     </main>
   )
--- Insta-Frontend/src/features/auth/pages/Register.jsx
+++ Insta-Frontend/src/features/auth/pages/Register.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router'
+import axios from 'axios'
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("second")
+    const [password, setPassword] = useState("")
+    
+    async function handleSubmit(e){
+        e.preventDefault()
+        const response = await axios.post("http://localhost:3000/api/auth/register", { 
+            username: username,
+            email: email,
+            password: password
+        },{
+            withCredentials: true
+        })
+
+        console.log(response.data)
+    }
+
+
+    return (
+        <main>
+            <div className="form-container">
+                <h1>INSTY</h1>
+                <h2>Register</h2>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        onInput={(e)=>{setEmail(e.target.value)}} 
+                        type='text' 
+                        name='email' 
+                        placeholder='Enter Email Address' required />
+                    <input 
+                        onInput={(e)=>{setUsername(e.target.value)}} 
+                        type='text' 
+                        name='username' 
+                        placeholder='Enter Username' required />
+                    <input 
+                        onInput={(e)=>{setPassword(e.target.value)}} 
+                        type='password' 
+                        name='password' 
+                        placeholder='Enter Password' required />
+                    <button type='submit'>Register</button>
+                </form>
+                <p className='toggleForm'>Already have an account? <Link className="link" to="/login">Login</Link></p>
+            </div>
+
+        </main>
+    )
 }
 
 export default Register
